5bb942f8c716ac79c9cc4ed19a086cc9a4778ace
mininvn <khaiminhg@gmail.com>
2025-03-13 13:57:24 +0700

    implement model processing in flask app

--- flask-app/server.py
+++ flask-app/server.py
@@ -2,7 +2,21 @@ from flask import Flask, request
 import base64
 import sys
 
+from ..benchmark.modules.utils import (
+  read_yaml_file
+)
+from ..benchmark.modules.model import YOLOv8
+
 app = Flask(__name__)
+
+def init_model():
+  config_file = os.path.abspath(r"../benchmark/configs/model.yaml")
+  config = read_yaml_file(config_file)
+  model = YOLOv8(config["model"])
+
+  return model
+
+model = init_model()
 
 def image_from_base64(base64_string):
   return base64.b64decode(base64_string)
@@ -18,17 +32,27 @@ def model_detection():
   image2 = image_from_base64(cam2)
   image3 = image_from_base64(cam3)
 
-  # TODO: model processing
+  predictions = model.infer(image2)
+  print('data', predictions, flush=True)
 
-  print('data', data, flush=True)
+  predictions_dict = {}
 
-  return {
-    "total": 3,
-    "items": [
-      { "id": 1, "count": 1 },
-      { "id": 3, "count": 2 }
-    ]
+  for prediction in predictions:
+    class_id = prediction.cls_id
+    if (class_id not in predictions_dict):
+      predictions_dict[class_id] = 0
+    predictions_dict[class_id] += 1
+
+  res = {
+    "total": len(predictions),
+    "items": []
   }
+
+  for class_id in predictions_dict:
+    count = predictions_dict[class_id]
+    res.items.append({ "id": class_id, count: count })
+
+  return res
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0')
