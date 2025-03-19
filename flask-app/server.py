7ec5bfad8c37e48e95290462652d04b116e49e08
mininvn <khaiminhg@gmail.com>
2025-03-19 15:30:12 +0700

    model in flask app

--- flask-app/server.py
+++ flask-app/server.py
@@ -1,16 +1,19 @@
 from flask import Flask, request
 import base64
 import sys
+import os
+import numpy as np
+import cv2
 
-from ..benchmark.modules.utils import (
+from src.utils import (
   read_yaml_file
 )
-from ..benchmark.modules.model import YOLOv8
+from src.model import YOLOv8
 
 app = Flask(__name__)
 
 def init_model():
-  config_file = os.path.abspath(r"../benchmark/configs/model.yaml")
+  config_file = os.path.abspath(r"./config/model.yaml")
   config = read_yaml_file(config_file)
   model = YOLOv8(config["model"])
 
@@ -19,7 +22,9 @@ def init_model():
 model = init_model()
 
 def image_from_base64(base64_string):
-  return base64.b64decode(base64_string)
+  nparr = np.frombuffer(base64.b64decode(base64_string), np.uint8)
+  img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+  return img
 
 @app.route("/model", methods = ["POST"])
 def model_detection():
@@ -33,24 +38,23 @@ def model_detection():
   image3 = image_from_base64(cam3)
 
   predictions = model.infer(image2)
-  print('data', predictions, flush=True)
 
   predictions_dict = {}
 
   for prediction in predictions:
-    class_id = prediction.cls_id
+    class_id = prediction["cls_id"]
     if (class_id not in predictions_dict):
       predictions_dict[class_id] = 0
     predictions_dict[class_id] += 1
 
   res = {
     "total": len(predictions),
-    "items": []
+    "products": []
   }
 
   for class_id in predictions_dict:
     count = predictions_dict[class_id]
-    res.items.append({ "id": class_id, count: count })
+    res["products"].append({ "id": int(class_id), "count": int(count) })
 
   return res
 
