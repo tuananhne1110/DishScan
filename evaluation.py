cc677f8462a5cd3afeebfd053064dd207099fa20
mininvn <khaiminhg@gmail.com>
2025-04-29 14:40:41 +0700

    use test dataset for evaluation

--- evaluation.py
+++ evaluation.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO(r'./checkpoints/dish_scan_v6.pt') 
 
-metrics = model.val(data=r'./datasets/data.yaml', conf=0.8, iou=0.8, save_json=True)
+metrics = model.val(data=r'./datasets/data.yaml', conf=0.8, iou=0.8, split="test", save_json=True)
 
 metrics.confusion_matrix.plot(
     normalize=True,
